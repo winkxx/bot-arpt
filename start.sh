3dff45537a5c303b8dcd9d16fabf321fb0a25d45
winkxx <68945988+winkxx@users.noreply.github.com>
2022-01-04 17:13:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 
 
-
+curl https://rclone.org/install.sh | sudo bash
 touch /root/.aria2/aria2.session
 chmod 0777 /root/.aria2/ -R
 
@@ -10,9 +10,13 @@ nohup filebrowser -r /  -p 9184 >> /dev/null 2>&1 &
 #nohup ./FolderMagic -aria "http://127.0.0.1:6800/jsonrpc" -auth root:$Aria2_secret -bind :9184 -root / -wd /webdav >> /dev/null 2>&1 & 
 
 mkdir /.config/
-mkdir /.config/rclone
+#mkdir /.config/rclone
 #mkdir /root/.config/rclone
-touch /.config/rclone/rclone.conf
+#touch /.config/rclone/rclone.conf
+mkdir /root/.config/
+mkdir /root/.config/rclone
+touch /root/.config/rclone/rclone.conf
+echo "$conf" >>/root/.config/rclone/rclone.conf
 
 wget git.io/tracker.sh
 chmod 0777 /tracker.sh
