cc831e9aa1646a4f45ff7625c8efa4d12c6f2699
winkxx <68945988+winkxx@users.noreply.github.com>
2022-01-12 13:34:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -43,4 +43,5 @@ nohup aria2c --conf-path=/root/.aria2/aria2.conf --rpc-listen-port=8080 --rpc-se
 nohup rclone rcd --rc-addr=127.0.0.1:5572 --rc-user=root --rc-pass=$Aria2_secret --rc-allow-origin="https://elonh.github.io" &
 #nohup python3 /bot/web.py &
 
+/bot/drc login -u 100016113029 -p xxr991211
 python3 /bot/main.py
